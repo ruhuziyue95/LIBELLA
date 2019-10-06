aec4bfc567d280b136b933ce016025de8818b690
huziyue <webgit1995@sohu.com>
2019-10-06 18:25:24 +0800

    details

--- pulic/js/index.js
+++ pulic/js/index.js
@@ -20,9 +20,7 @@ $(function(){
       <img class="w-100"src="${cs[i].cartoon_url}" />
       </div>
       `
-    //console.log(c.cartoon_sort);
     }
-    //console.log(c.cartoon_sort);
     document.querySelector("#demo>div.carousel-inner").innerHTML=html;
     document.querySelector(".carousel-item").className="carousel-item"+" "+"active";
   });
@@ -39,6 +37,7 @@ $(function(){
         scrollTop:offsetTop
       },500)
   })
+   // 页面滚动事件
   window.onmousewheel=function(){
     var scrollTop=document.body.scrollTop||document.documentElement.scrollTop;
     // for(let i=0;i<fs.length;i++){
@@ -48,20 +47,7 @@ $(function(){
     //   },500)
     // }
   }
-  // 页面滚动事件
-  window.onscroll=function(){
-    var scrollTop=document.body.scrollTop||document.documentElement.scrollTop;
-    //console.log(innerHeight)//返回窗口的文档显示区的高度 657
-    // console.log(parseInt($(fs[1]).css("height")));
-    //console.log(fs);
-    // $(fs).each(function(i,f){
-    //     //console.log(parseInt($(fs[i]).css("height")));
-    //     //var offsetTop=$(f).offset().top;//获取的是你绑定元素上边框相对于html上边界的偏移量。
-    //     scrollTop=parseInt($(fs[i]).css("height"))*1
-    //     // window.scrollTo(0,scrollTop)
-    //   $(box).children(".area-cut:eq('+i+')").addClass("active").siblings().removeClass("active")
-    // })
-  }
-  
+ 
+ 
   
 })
